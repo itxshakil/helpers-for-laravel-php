e68cb4ba842026a0c9246dc2dc10b4b762c535da
Shakil Alam <30234430+itxshakil@users.noreply.github.com>
2023-08-10 08:31:01 +0000

    Register TimeTrackingService as singleton
    Signed-off-by: Shakil Alam <30234430+itxshakil@users.noreply.github.com>

---
+++ Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Support\Services\TimeTrackingService;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(TimeTrackingService::class, function () {
+            return new TimeTrackingService();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
